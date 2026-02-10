1344b36933c5a74e19349db3dbe5ebcb3453f75e
ConcentradoClavedeCartera <DPO.CGPII.IPN@gmail.com>
2026-02-09 23:02:40 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1029,6 +1029,12 @@ setTimeout(() => {
                     this.reportValidity(); // muestra mensajes
                     return;  
                 }
+                 const confirmar = confirm("¿Estás seguro de que deseas enviar el formulario?");
+
+                if (!confirmar) {
+                  // ❌ Usuario canceló
+                  return;
+                }
                 const formData = new FormData(this);
                 const selectsTexto = [
                       "secretaria",
@@ -1068,6 +1074,7 @@ setTimeout(() => {
   })
   .catch(() => alert("Error al enviar"));
 });
+
 
 
 
